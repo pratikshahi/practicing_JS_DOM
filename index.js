eb33597dff72b4ddda4889085d127d67fdd08663
Pratik Shahi <shahipr0@gmail.com>
2021-03-16 17:16:42 +0530

    using basic DOM features

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".list")[2].innerHTML="Pratik";
+document.querySelector("li a").style.color="red";
+document.querySelector("button").style.backgroundColor="yellow";
+// Keeping styles in stylessheet
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.remove("huge");
+document.querySelector("h1").classList.toggle("huge");
+//Html using JS
+document.querySelector("h1").innerHTML="<em>HTml usning JS</em>";
+document.querySelector("li a").textContent="using text content";
